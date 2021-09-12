27b6513f232263f12538cb55f48a2c8d78cc361f
phamkyquan <quanbn69@gmail.com>
2021-09-12 22:50:46 +0700

    update

--- demo/src/main/java/Client/KeyClient.java
+++
@@ -1,9 +0,0 @@
-package Client;
-
-import java.security.PrivateKey;
-
-public class KeyClient {
-	protected static PrivateKey privateKeyC;
-	protected static byte[] publicKeyS;
-	protected static boolean check = false;
-}
--- demo/src/main/java/Server/KeyServer.java
+++
@@ -1,9 +0,0 @@
-package Server;
-
-import java.security.PrivateKey;
-
-public class KeyServer {
-	protected static byte[] publicKeyC;
-	protected static PrivateKey PrivateKeyS;
-	protected static boolean check = false;
-}
